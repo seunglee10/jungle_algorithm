77db74686de43a7701bead9a463bba6da1ee7849
seunglee10 <purpleleeyu@gmail.com>
2026-03-08 22:11:14 +0900

    백준 최댓값 2562번 #17

--- week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,36 @@
 # 파이썬 문법 - 최댓값 (백준 브론즈3)
 # 문제 링크: https://www.acmicpc.net/problem/2562
+
+
+"""
+입력예제
+3
+29
+38
+12
+57
+74
+40
+85
+61
+
+출력예제
+85
+5
+
+"""
+
+# import sys
+# data = list(map(int, sys.stdin.read().split()))
+
+# data = [int(input()) for _ in range(9)]
+
+data = [int(input()) for _ in range(9)]
+max_number = data[0]
+max_idx = 0
+for idx, number in enumerate(data):
+    if number > max_number:
+        max_number = number
+        max_idx = idx
+print(max_number)
+print(max_idx + 1)
