69b7946a01c63cce1e79cc4cf9031e098d505de8
seunglee10 <purpleleeyu@gmail.com>
2026-03-08 23:52:09 +0900

    백준 문자열 반복 2675번 # 19

--- week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
+++ week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
@@ -1,2 +1,18 @@
 # 문자열 - 문자열 반복 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/2675
+
+
+"""
+문자열 S를 입력받은 후에, 각 문자를 R번 반복해 새 문자열 P를 만든 후 출력
+
+"""
+
+data = "ABC"
+count = 3
+
+# count, data= input().split()
+
+word = ""
+for w in data:
+    word += w * count
+print(word)
