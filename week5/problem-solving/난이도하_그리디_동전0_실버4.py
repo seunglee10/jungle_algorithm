c2e97f11ed71f2263409680ff8dab52cc76e18fd
seunglee10 <purpleleeyu@gmail.com>
2026-03-29 01:28:46 +0900

    백준_그리디_동전0_#115

--- week5/problem-solving/난이도하_그리디_동전0_실버4.py
+++ week5/problem-solving/난이도하_그리디_동전0_실버4.py
@@ -1,2 +1,18 @@
 # 그리디 - 동전 0 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/11047
+
+n, k = map(int, input().split())
+
+coins = []
+for i in range(n):
+    coins.append(int(input()))
+# print(coins)
+
+count = 0
+for coin in coins[::-1]:
+    if coin > k:
+        pass
+    elif coin <= k:
+        count += k // coin
+        k = k % coin
+print(count)
