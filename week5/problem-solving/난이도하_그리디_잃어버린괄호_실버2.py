06223a296d9a780ca92907e9438aec9c087154ba
seunglee10 <purpleleeyu@gmail.com>
2026-03-29 16:38:30 +0900

    백준_그리디_잃어버린괄호 #116

--- week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
+++ week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
@@ -2,6 +2,22 @@
 # 문제 링크: https://www.acmicpc.net/problem/1541
 
 
-data = input().strip()
-strings = data.split('-')
-print(strings)
+n = input().strip()
+parts = n.split("-")
+# print(parts) # ['55', '50+40']
+
+# 
+first_sum = 0
+save = 0
+result = 0
+part_sum = []
+for part in parts:
+    part_sum.append(sum(list(map(int, part.split("+")))))  # nums = [55], [50, 40]
+    # print(part_sum) # [55, 90]
+# print(part_sum)
+result = part_sum[0]
+
+for i in range(1,len(part_sum)):
+    result -= part_sum[i]
+        
+print(result)
