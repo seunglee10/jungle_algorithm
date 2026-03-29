0793eb313477b6ac7674c8f04554568b032af47f
seunglee10 <purpleleeyu@gmail.com>
2026-03-29 09:42:28 +0900

    백준_dp_피보나치수 #113

--- week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
+++ week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
@@ -1,2 +1,13 @@
 # DP - 피보나치 수 2 (백준 브론즈 1)
 # 문제 링크: https://www.acmicpc.net/problem/2748
+
+data = int(input())
+
+dp = [0] * (data + 1)
+if data >= 1:
+    dp[1] = 1
+
+for i in range(2, data + 1):
+    dp[i] = dp[i - 1] + dp[i - 2]
+
+print(dp[data])
