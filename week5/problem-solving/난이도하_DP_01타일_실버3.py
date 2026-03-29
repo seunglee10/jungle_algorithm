f7154a8fa7cf5d7ce57f0c6e8da0940ce5bc8739
seunglee10 <purpleleeyu@gmail.com>
2026-03-29 10:50:24 +0900

    백준_dp_타일 #114

--- week5/problem-solving/난이도하_DP_01타일_실버3.py
+++ week5/problem-solving/난이도하_DP_01타일_실버3.py
@@ -1,2 +1,15 @@
 # DP - 01타일 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/1904
+
+data = int(input())
+dp = [0] * (data + 1)
+
+if data >= 1:
+    dp[1] = 1
+if data >= 2:
+    dp[2] = 2
+
+for i in range(3, data + 1):
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 15746
+
+print(dp[data])
